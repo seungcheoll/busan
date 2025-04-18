7d20a12ead428eaae738f5ca22b7c252616e7adb
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-04-18 14:14:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -322,7 +322,7 @@ if chatbot:
 
     # 챗봇 헤더 UI
     st.markdown("""
-        <div style='background-color:#f9f9f9; padding:10px 10px; border-radius:12px; border:1px solid #ddd; 
+        <div style='background-color:#f9f9f9; padding:0px 0px; border-radius:12px; border:1px solid #ddd; 
                     width:20%; margin: 0 auto; text-align: center;'>
             <h1 style='margin:0; font-size:24px; display: flex; align-items: center; justify-content: center; gap: 10px;'>
                 <img src='https://raw.githubusercontent.com/seungcheoll/busan/main/GPT_image.png' 
