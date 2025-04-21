c3da8b864987325c6b07f88565bd605dd2b2a5b6
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-04-21 15:34:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,9 +240,7 @@ if info:
               </ul>
             </li>
           </ul>
-          
           <div style="height:30px;"></div>
-          
           <h4>2️⃣ Job‑Bu Chatbot (상담형 챗봇)</h4>
           <ul>
             <li>🤖 기업 추천 이후 추가 질문 가능</li>
