7c97783e43b2794fc6838e2388c0e2854320772f
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-05-02 16:09:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -489,7 +489,8 @@ if job_rag:
             # 👉 Expander에 표시
             for _, row in matched_df_by_gpt.iterrows():
                 content_to_gpt={}
-                with st.expander(f"**{row['회사명']}** 상세 정보):
+                name=row['회사명']
+                with st.expander(f"**{name}** 상세 정보):
                     content = format_row(row)
                     st.session_state.content_to_gpt.append(content)
                     st.write(content)
