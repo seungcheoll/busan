71aa8dfc9a5a8edacdf8ad113ce3041808089f03
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-05-09 09:50:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ st.markdown("""
 
 def authenticate():
     if not st.session_state.get("authenticated", False):
-        # st.markdown('<div class="login-container">', unsafe_allow_html=True)
+        st.markdown('<div class="login-container">', unsafe_allow_html=True)
         # st.markdown('<div class="login-card">', unsafe_allow_html=True)
         st.markdown('<h2>🚀 지금 바로 JOBBUSAN을 시작하세요!</h2>', unsafe_allow_html=True)
 
