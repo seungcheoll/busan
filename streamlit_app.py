714428ab4634de99725f1e4ccbd1c4b17999523a
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-05-09 14:34:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.markdown("""
         border-radius: 12px;
         box-shadow: 0 4px 12px rgba(0,0,0,0.1);
         padding: 40px;
-        width: 320px;
+        width: 100px;
         text-align: center;
       }
       .login-card input {
