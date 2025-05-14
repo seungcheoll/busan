4b9c60629c2d3aeb86baf161ee2fd98bac5136fe
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-05-14 12:04:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,8 @@ def check_login():
             st.markdown('<h2 style="text-align:center;">😊 JOB-IS에 오신 걸 환영합니다!</h2>', unsafe_allow_html=True)
 
             with st.form("login_form"):
-                pw = st.text_input(" Login이 필요한 서비스 입니다.", type="password", placeholder="로그인 코드를 입력하세요")
-                submitted = st.form_submit_button("확인")
+                pw = st.text_input("", type="password",label_visibility="collapsed", placeholder="로그인 코드를 입력하세요")
+                submitted = st.form_submit_button("로그인")
                 if submitted:
                     if pw == st.secrets["general"]["APP_PASSWORD"]:
                         st.session_state.authenticated = True
