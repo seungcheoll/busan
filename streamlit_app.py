b35e52d67d24dff38a0ec1a8940ae8d86c21e5e7
seungcheoll <137670706+seungcheoll@users.noreply.github.com>
2025-06-19 15:42:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -362,13 +362,13 @@ if Guide:
     </style>
 
     <div class="gbox">
-    st.markdown("""
-    <div style="text-align: center; width: 100%; margin-bottom: 10px;">
-        <p style="font-weight: bold;">
-          왼쪽 위 <span style="color: red; font-size: 18px;">사이드 바(&raquo;&raquo;)</span> 메뉴를 펼쳐 진행하세요!
-        </p>
-    </div>
-    """, unsafe_allow_html=True)
+st.markdown("""
+<div style="text-align: center; width: 100%; margin-bottom: 10px;">
+    <p style="font-weight: bold;">
+      왼쪽 위 <span style="color: red; font-size: 18px;">사이드 바(&raquo;&raquo;)</span> 메뉴를 펼쳐 진행하세요!
+    </p>
+</div>
+""", unsafe_allow_html=True)
       <div class="split">
         <div class="image-section">
           <p style="font-size:30px; font-weight:bold; text-align:center; margin:0;">
